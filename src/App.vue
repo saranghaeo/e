30ff812f6b489fbd621e6277632cc9c8854ab498
saranghaeo <ddesuofficial@gmail.com>
2023-04-08 20:01:52 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<template>
+  <header class="header">
+
+    <nav class="navigation start">
+      <router-link class="nav-link" to="/">Игроки</router-link>
+      <router-link class="nav-link" to="/">Тренировки</router-link>
+      <router-link class="nav-link" to="/">Генератор</router-link>
+      <router-link class="nav-link" to="/">Новости</router-link>
+      <router-link class="nav-link" to="/">Гайды</router-link>
+    </nav>
+
+    <nav class="logo">
+      <router-link class="logo-link" to="/">Exec</router-link>
+    </nav>
+
+    <nav class="navigation end">
+      <router-link class="nav-link" to="/">Войти</router-link>
+      <router-link class="nav-link" to="/">Регистрация</router-link>
+    </nav>
+
+  </header>
+
+  <router-view></router-view>
+
+  <footer>
+
+
+    <div class="container">
+
+      <h1 class="logo-footer">Exec</h1>
+
+      <div class="content">
+
+        <div class="navigation-footer">
+          <p class="title-link">Настройки</p>
+          <nav class="nav-footer">
+            <router-link class="link-footer" to="/">Игроки</router-link>
+            <router-link class="link-footer" to="/">Генератор прицела</router-link>
+          </nav>
+        </div>
+
+        <div class="navigation-footer">
+          <p class="title-link">Тренировки и гайды</p>
+          <nav class="nav-footer">
+            <router-link class="link-footer" to="/">Игроки</router-link>
+            <router-link class="link-footer" to="/">Игроки</router-link>
+          </nav>
+        </div>
+
+        <div class="navigation-footer">
+          <p class="title-link">Новости</p>
+          <nav class="nav-footer">
+            <router-link class="link-footer" to="/">Игроки</router-link>
+            <router-link class="link-footer" to="/">Игроки</router-link>
+          </nav>
+        </div>
+
+      </div>
+
+      <div class="social-media">
+        <img src="@/assets/img/social/footer/discord.png" alt="discord">
+        <img src="@/assets/img/social/footer/vkontakte.png" alt="vkontakte">
+        <img src="@/assets/img/social/footer/steam.png" alt="steam">
+        <img src="@/assets/img/social/footer/github.png" alt="github">
+      </div>
+    </div>
+
+    <p class="copyright">Copyright© 2023</p>
+
+  </footer>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: white;
+}
+
+body {
+  background: #000000;
+}
+
+.header {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 20px;
+}
+
+.logo {
+  position: absolute;
+  width: 160px;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+}
+
+.nav-link {
+  font-size: 16px;
+  color: #fff;
+  text-decoration: none;
+}
+
+.logo-link {
+  font-size: 36px;
+  font-weight: 700;
+  color: #fff;
+  text-decoration: none;
+}
+
+nav a {
+  font-weight: 400;
+  margin: 0 20px;
+}
+
+nav a.router-link-exact-active {}
+
+footer {
+  padding: 30px 20px;
+}
+
+.logo-footer {
+  font-size: 48px;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 30px 50px;
+}
+
+.content {
+  display: flex;
+}
+
+.navigation-footer{
+  text-align: center;
+}
+
+.nav-footer {
+  display: flex;
+  flex-direction: column;
+}
+
+.title-link {
+  font-size: 16px;
+  font-weight: 600;
+  padding: 10px 0;
+}
+
+.link-footer {
+  font-size: 15px;
+  color: #fff;
+  text-decoration: none;
+  margin: 0px 80px;
+}
+
+.social-media img {
+  padding: 0 5px;
+}
+
+.copyright {
+  font-size: 15px;
+  font-weight: 600px;
+  text-align: center;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="content">
+        <img class="img-content" src="@/assets/img/main_page/left-1.png" alt="left">
+        <img class="img-content mid" src="@/assets/img/main_page/mid-1.png" alt="mid">
+        <img class="img-content" src="@/assets/img/main_page/right-1.png" alt="right">
+
+        <div class="text-btn">
+            <h1 class="title-main">Храни и делись своими настройками с другими пользователями</h1>
+            <div class="button-join">
+                <button class="join">Присоединяйся</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+.content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 50px 20px;
+}
+
+.img-content {
+    position: relative;
+    width: 300px;
+    height: 570px;
+    filter: brightness(80%)
+}
+
+.content:hover span{
+    color: rgba(0, 0, 0, 0.5);
+}
+
+.mid {
+    width: 460px;
+    height: 700px;
+    margin: 0 20px;
+}
+
+.text-btn {
+    position: absolute;
+    margin: 0 auto;
+    max-width: 680px;
+}
+
+.title-main {
+    font-size: 48px;
+    text-align: center;
+}
+
+.button-join {
+    display: flex;
+    justify-content: center;
+    margin-top: 90px;
+}
+
+.join {
+    font-size: 20px;
+    font-weight: 500px;
+    padding: 0 15px;
+    background: none;
+    color: #fff;
+    border: 1px solid #FFFFFF;
+}
+</style>
