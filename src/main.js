e0b10152fb614c0a1e1f0104a879b5b95d120cf5
saranghaeo <ddesuofficial@gmail.com>
2023-04-17 11:21:52 +0300

    5345

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from '@/router'
+import router from './router/router';
+import passport from 'passport';
+
+require('./passport');
+
+passport.deserializeUser((user, done) => {
+    window.user = user;
+    done(null, user);
+  });
+  
 
 const app = createApp(App)
 const pinia = createPinia()
